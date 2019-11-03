2bdc01d1771eae5fa941489af2bc725a539a0ba2
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-03 21:17:01 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+yes im 
 # Square Array
 
 ## Objectives
--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+#new_array=[]
+  array.each do |num|
+# num **2
+# return num << []
+    
+end
 end
